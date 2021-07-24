f9034bebabd40be7f817a63d02bf4053a6621746
LienPaul <zinfu1111@gmail.com>
2021-07-24 22:54:25 +0800

    Initial Commit

---
+++ TenYearChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TenYearChallenge
+//
+//  Created by 連振甫 on 2021/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
